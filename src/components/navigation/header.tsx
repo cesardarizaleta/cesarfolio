e27379c67337d8673dcb2cbe620d44a77dfe6993
cesardarizaleta <cesardarizaleta@gmail.com>
2025-02-25 01:28:44 -0400

    Add Adapter (vercel)

--- src/components/navigation/header.tsx
+++ src/components/navigation/header.tsx
@@ -4,13 +4,13 @@ import {Tooltip, Button} from "@heroui/react";
 export default function Header(){
     return (
         <header className="fixed top-5 z-40 flex p-4 gap-4 bg-[#f3f3f310] rounded-xl backdrop-blur-md">
-            <Tooltip closeDelay={0} content="Proyectos" className="bg-[#00000080] rounded-md">
+            <Tooltip closeDelay={0} content="Proyectos" className="bg-[#00000080] p-2 rounded-md">
                 <Folder className="cursor-pointer" color="#ffffff" size="32" />
             </Tooltip>
-            <Tooltip closeDelay={0} content="Inicio" className="bg-[#00000080] rounded-md">
+            <Tooltip closeDelay={0} content="Inicio" className="bg-[#00000080] p-2 rounded-md">
                 <House className="cursor-pointer" color="#ffffff" size="32" />
             </Tooltip>
-            <Tooltip closeDelay={0} content="Tecnologias" className="bg-[#00000080] rounded-md">
+            <Tooltip closeDelay={0} content="Tecnologias" className="bg-[#00000080] p-2 rounded-md">
                 <Webhook className="cursor-pointer" color="#ffffff" size="32" />
             </Tooltip>
         </header>
